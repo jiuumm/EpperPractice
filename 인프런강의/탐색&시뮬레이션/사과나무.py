058c0a349220a7a5df833effce46770bbfd80525
Ji Young <jiyoung6301@gmail.com>
2023-08-16 10:23:44 +0900

    Create 사과나무.py

---
+++ 인프런강의/탐색&시뮬레이션/사과나무.py
@@ -0,0 +1,13 @@
+n= int(input())
+A= [list(map(int, input().split())) for _ in range(n)]
+s=e=n//2
+sum=0
+for i in range(n):
+    for j in range(s, e+1): #j:s...e
+        sum+=A[i][j]
+    if i<n//2:
+        s-=1
+        e+=1
+    else:
+        s+=1
+        e-=1   
