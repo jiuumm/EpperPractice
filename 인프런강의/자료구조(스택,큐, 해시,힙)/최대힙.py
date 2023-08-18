5923b43dbb1c123ea6285fb6381ebf8866b4c70f
Ji Young <jiyoung6301@gmail.com>
2023-08-18 18:32:28 +0900

    Create 최대힙.py

---
+++ 인프런강의/자료구조(스택,큐, 해시,힙)/최대힙.py
@@ -0,0 +1,15 @@
+import heapq as hq
+
+a= []
+while True:
+    n= int(input())
+    if n==-1: 
+        break
+    if n ==0 :
+        if len(a)==0: #만약 비어있다면
+            print(-1) #-1 출력
+        else: #비어있지 않다면 루트 노드 값 출력
+            print(-hq.heappop(a))
+    else: 
+        hq.heappush(a, -n) #a라는 리스트에 n값을 push
+                
